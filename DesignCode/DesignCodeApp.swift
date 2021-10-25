3c72673b46fa2ee60bc906a331f680b1cf17c654
Devesh Tyagi <tyagi77@outlook.com>
2021-10-25 11:56:47 +0530

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Devesh Tyagi on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
