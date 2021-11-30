458eafa4cf55da2ac5a152efb4971870e4b89a4b
Devesh Tyagi <tyagi77@outlook.com>
2021-11-30 16:44:37 +0530

    API And JSON

---
+++ DesignCode/Data.swift
@@ -0,0 +1,34 @@
+//
+//  Data.swift
+//  DesignCode
+//
+//  Created by Devesh Tyagi on 30/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Post: Codable , Identifiable {
+    
+    let id = UUID()
+    var title: String
+    var body: String
+    
+}
+
+
+class Api {
+    func getPosts(completion : @escaping ([Post]) -> ()){
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _ , err in
+            guard let data = data else { return }
+            let posts = try! JSONDecoder().decode([Post].self, from: data)
+            DispatchQueue.main.async {
+                completion(posts)
+            }
+            
+        }
+        .resume()
+    }
+}
